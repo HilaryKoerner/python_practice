b0dfbf73758e0814e5ed1a501c6345914f3950ee
Hilary <hilary.koerner@gmail.com>
2021-07-05 16:03:47 -0500

    added hello world and variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+# = is an assignment variable
+
+#create a variable string
+job = "coder"
+
+#create a variable with an integer
+years = 1
+
+#create a variable with the boolean value of True
+expert_status = True
+
+#print statements with the variables
+print("Hilary is a new" + job)
+
