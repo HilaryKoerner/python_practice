584ef5398d66b84be64e7ea4aaf29f7d5013d2a9
Hilary <hilary.koerner@gmail.com>
2021-07-05 16:24:26 -0500

    second variable practice

---
+++ variables_2.py
@@ -0,0 +1,10 @@
+name = "Hilary"
+country = "USA"
+age = 35
+hourly_wage = 100
+satisfied = True
+daily_wage = hourly_wage * 8
+
+print("The new employee is " + name + " and she is " + str(age)+ ". Her daily wage is " + str(daily_wage) + ".")
+
+print(f"Satisfied: {satisfied}")
