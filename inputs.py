0d02a860a18613391ccd63120b4aed72d38997dd
Hilary <hilary.koerner@gmail.com>
2021-07-05 16:43:46 -0500

    inputs

---
+++ inputs.py
@@ -0,0 +1,7 @@
+#in command prompt, input will display the string and collect what the user types
+name = input("What is your name?")
+age = int(input("How old are you?"))
+
+#once the input are collected, we can print the inputs
+print(f"My name is {name} .")
+print(f"I am {age} and I will be {age+1} next year.")
