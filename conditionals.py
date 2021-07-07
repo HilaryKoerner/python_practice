5fea471b49f5ce1528d12eb611c2ceab213a4ea6
Hilary <hilary.koerner@gmail.com>
2021-07-06 19:18:38 -0500

    nested

--- conditionals.py
+++ conditionals.py
@@ -10,4 +10,21 @@ if x == 1:
 if y != 1:
     print("y is not equal to 1")
 
+if x < y:
+    print("x is less than y")
 
+if y>x:
+    print("y is greater than x")
+
+if x == 1 and y == 10:
+    print("x is 1 and y is 10") 
+
+if x < 10:
+    if y <5:
+        print("x is less than 10, y is less than 5")
+    elif y == 5:
+        print ("x is less than 10 and y is equal to 5")
+    else:
+        print("x is less than 10 and y is greater than 5")
+
+        
