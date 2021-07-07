c1db5ade4c9cbfac07298054098b0dee61c68e50
Hilary <hilary.koerner@gmail.com>
2021-07-06 19:58:22 -0500

    loop string, ranges

--- loops.py
+++ loops.py
@@ -1,2 +1,22 @@
+
+# range is the bottom number upto the last number in parenthesis. In this case, bottom number is 1 and goes up to 4, not 5
+# this will print 1, 2, 3, 4 (up to 5, not including 5)
 for x in range(5):
     print(x)
+
+print("---------------------")    
+
+for x in range(2,7):
+    print (x)
+
+print("---------------------")   
+
+word = "coding"
+for letter in word:
+    print(letter)
+
+print("---------------------")   
+
+heroes = ["Dolly Parton", "Nick Offerman", "Frida Kahlo"]
+for hero in heroes:
+    print(hero)
