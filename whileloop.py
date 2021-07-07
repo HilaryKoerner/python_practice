1649d6d72a4867ebf4d3f142e88561dc21bf25e6
Hilary <hilary.koerner@gmail.com>
2021-07-06 20:23:21 -0500

    while loops

---
+++ whileloop.py
@@ -0,0 +1,13 @@
+#initial variable
+
+user_play = "y"
+
+while user_play == "y":
+    user_number = int(input("Enter a number"))
+
+    #added in the 1 and +1 to account for range (which goes from 0 up until the user number)
+    for x in range(1, user_number + 1):
+        print (x)
+    
+    #print question to continue or kill the loop
+    user_play = input("continue: (y)es or (n)no?")
