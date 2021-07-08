67c4f09136bf76e01c07615366661a30c74e662c
Hilary <hilary.koerner@gmail.com>
2021-07-07 20:26:28 -0500

    cast int

--- whatsyourage.py
+++ whatsyourage.py
@@ -4,10 +4,10 @@ name = (input("What is your name?"))
 
 print(f"Hello {name}!")
 
-age = input("What's your age?")
+age = int(input("What's your age?"))
 
 if age >= 40:
     print("You're over the hill")
 
 else:
-    print("You've got your whole life in front of you")
+    print("You've got your whole life in front of you.")
