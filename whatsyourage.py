32a602ea04eb95100f1470585c36c0b17eecbef8
Hilary <hilary.koerner@gmail.com>
2021-07-07 20:25:00 -0500

    name and age

---
+++ whatsyourage.py
@@ -0,0 +1,13 @@
+print("Hello User")
+
+name = (input("What is your name?"))
+
+print(f"Hello {name}!")
+
+age = input("What's your age?")
+
+if age >= 40:
+    print("You're over the hill")
+
+else:
+    print("You've got your whole life in front of you")
